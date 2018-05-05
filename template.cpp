77145a8487cbdb18485fc9c70bd6495eedeae2da
jvazquez96 <jorgevzqz6@gmail.com>
2018-05-05 12:24:03 -0500

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <stack>
+#include <queue>
+#include <map>
+#include <stdio.h>
+
+using namespace std;
+
+#define mem(x, val) memset((x), (val), sizeof(x))
+#define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
+#define REP(i, a, b) for(int i=int(a); i<=int(b); i++)
+#define pb push_back
+#define mk make_pair
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef long long ll;
+typedef vector<long long> vll;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vllll;
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	return 0;
+}
